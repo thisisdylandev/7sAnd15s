dd75dbfb9dbccb2caa5bc7dddda0125ec8fb5eaa
thisisdylandev <dylan@ctrlaltdylan.com>
2021-08-07 11:05:25 -0500

    feat: updating scaffolding to use pages meat-pie needs

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import {
   IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { calendarClearSharp, peopleCircleSharp, personCircleOutline } from 'ionicons/icons';
 import { Redirect, Route } from 'react-router-dom';
 
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Profile from './pages/Profile';
+import Schedule from './pages/Schedule';
+import Team from './pages/Team';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -39,31 +39,31 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/team">
+            <Team />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/schedule">
+            <Schedule />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route path="/profile">
+            <Profile />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/team" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="team" href="/team">
+            <IonIcon icon={peopleCircleSharp} />
+            <IonLabel>Team</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="schedule" href="/schedule">
+            <IonIcon icon={calendarClearSharp} />
+            <IonLabel>Schedule</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="profile" href="/profile">
+            <IonIcon icon={personCircleOutline} />
+            <IonLabel>Profile</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
